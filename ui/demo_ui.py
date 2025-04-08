b51f8e34f0d1cac83fcb79a45c5d6399c6b11722
Ravilace <wujinjun65@gmail.com>
2025-04-08 16:14:29 +0800

    :lipstick: 使用MainWindow，并添加按钮

--- ui/demo_ui.py
+++ ui/demo_ui.py
@@ -1,8 +1,23 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
-app = QApplication([])
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
-window = QMainWindow()
-window.show()
 
-app.exec()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        self.setCentralWidget(button)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
