931fc13084650ecc0dc0a214215b4882bfbaeb85
Ravilace <wujinjun65@gmail.com>
2025-04-09 16:29:17 +0800

    :sparkles: connecting widgets together directly

--- ui/signal_practice.py
+++ ui/signal_practice.py
@@ -1,52 +1,26 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2025/4/9 15:23
-# @Author  : Ravilace
-# @File    : signal_practice.py
-# @Software: PyCharm
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+
 import sys
-from random import choice
-
-from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-
-window_titles = [
-    "My App",
-    "My App",
-    "Still My App",
-    "Still My App",
-    "What on earth",
-    "What on earth",
-    "This is surprising",
-    "This is surprising",
-    "Something went wrong"
-]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0
         self.setWindowTitle("My App")
-        self.setFixedSize(QSize(600, 600))
+        self.label = QLabel()
 
-        self.button = QPushButton("Press Me!")
-        self.button.clicked.connect(self.the_button_was_clicked)
-        self.windowTitleChanged.connect(self.the_window_title_changed)
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
 
-        self.setCentralWidget(self.button)
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print(f"Setting title: {new_window_title}")
-        self.setWindowTitle(new_window_title)
+        container = QWidget()
+        container.setLayout(layout)
 
-    def the_window_title_changed(self, window_title):
-        print(f"Window title changed: {window_title}")
-
-        if window_title == "Something went wrong":
-            self.button.setDisabled(True)
+        self.setCentralWidget(container)
 
 
 if __name__ == '__main__':
