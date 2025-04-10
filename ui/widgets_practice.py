673f689c197667a55b5e495d63f253d537781edb
Ravilace <wujinjun65@gmail.com>
2025-04-10 15:29:52 +0800

    :sparkles: QCheckBox practice

--- ui/widgets_practice.py
+++ ui/widgets_practice.py
@@ -1,23 +1,23 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("My App")
 
-        widget = QLabel("Hello")
-        font = widget.font()
-        font.setPointSize(30)
-        widget.setFont(font)
-        widget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        widget.setPixmap(QPixmap('otje.png'))
+        widget = QCheckBox("This is a checkbox")
+        widget.setCheckState(Qt.PartiallyChecked)
 
+        widget.stateChanged.connect(self.show_state)
         self.setCentralWidget(widget)
+
+    def show_state(self, s):
+        print(s == Qt.Checked)
+        print(s)
 
 
 if __name__ == '__main__':
