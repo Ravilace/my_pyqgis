89d36c219c3249925c4fca9395da53e63b66b2f1
Ravilace <wujinjun65@gmail.com>
2025-04-10 15:39:09 +0800

    :sparkles: QListWidget practice

--- ui/widgets_practice.py
+++ ui/widgets_practice.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QListWidget
 
 
 class MainWindow(QMainWindow):
@@ -8,21 +8,19 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My App")
 
-        widget = QComboBox()
+        widget = QListWidget()
         widget.addItems(["One", "Two", "Three"])
 
-        widget.currentIndexChanged.connect(self.index_changed)
+        widget.currentItemChanged.connect(self.index_changed)
         widget.currentTextChanged.connect(self.text_changed)
-        # widget.setEditable(True)
 
         self.setCentralWidget(widget)
 
     def index_changed(self, i):
-        print(i)
+        print(i.text())
 
     def text_changed(self, s):
         print(s)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
