553de9e922b611f9bd2c50dc4574f67401304835
Ravilace <wujinjun65@gmail.com>
2025-04-10 16:44:35 +0800

    :sparkles: QDiag practice

--- ui/widgets_practice.py
+++ ui/widgets_practice.py
@@ -1,7 +1,6 @@
 import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDial
 
 
 class MainWindow(QMainWindow):
@@ -9,11 +8,9 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My App")
 
-        widget = QSlider(Qt.Horizontal)
-        widget.setMinimum(-10)
-        widget.setMaximum(3)
-
-        widget.setSingleStep(3)
+        widget = QDial()
+        widget.setRange(-10, 100)
+        widget.setSingleStep(1)
 
         widget.valueChanged.connect(self.value_changed)
         widget.sliderMoved.connect(self.slider_position)
@@ -26,13 +23,13 @@ class MainWindow(QMainWindow):
         print(i)
 
     def slider_position(self, p):
-        print(f"position: {p}")
+        print("position", p)
 
     def slider_pressed(self):
         print("Pressed!")
 
     def slider_released(self):
-        print("Released")
+        print("Released!")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
