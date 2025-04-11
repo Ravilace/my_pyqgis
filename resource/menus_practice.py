76d395e60f922f49e0b8a75c4f3b9a62894580d4
Ravilace <wujinjun65@gmail.com>
2025-04-11 15:51:17 +0800

    :sparkles: multi widget in toolbar

--- resource/menus_practice.py
+++ resource/menus_practice.py
@@ -25,11 +25,22 @@ class MainWindow(QMainWindow):
         toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon("../resource/bug.png"), "Your button", self)
+        button_action = QAction(QIcon("../resource/bug.png"), "&Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.toolbar_button_clicked)
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+
+        button_action2 = QAction(QIcon("../resource/bug.png"), "Your &button2", self)
+        button_action2.setStatusTip("This is your button2")
+        button_action2.triggered.connect(self.toolbar_button_clicked)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
 
         self.setStatusBar(QStatusBar(self))
 
