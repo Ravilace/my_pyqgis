8b538ea5ce62bf6ab3ca055c0de7edc913e8318a
Ravilace <wujinjun65@gmail.com>
2025-04-11 15:46:27 +0800

    :sparkles: add checkable attr to button action

--- resource/menus_practice.py
+++ resource/menus_practice.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtWidgets import (
     QAction,
@@ -22,13 +22,16 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
 
-        button_action = QAction("Your button", self)
+        button_action = QAction(QIcon("../resource/bug.png"), "Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.toolbar_button_clicked)
-
+        button_action.setCheckable(True)
         toolbar.addAction(button_action)
+
+        self.setStatusBar(QStatusBar(self))
 
     def toolbar_button_clicked(self, s):
         print("click", s)
