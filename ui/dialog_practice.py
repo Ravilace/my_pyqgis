3c483fe95559554f66cf0f2250e150b89267f0ed
Ravilace <wujinjun65@gmail.com>
2025-04-14 09:54:01 +0800

    :tada: Dialog tada

---
+++ ui/dialog_practice.py
@@ -0,0 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+
+        self.setCentralWidget(button)
+
+
+    def button_clicked(self, s):
+        print("Click", s)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
